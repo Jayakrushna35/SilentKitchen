01f5a392eea3ac8fd32138e729176acc7d2b5fc4
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-06-03 11:39:03 +0530

    server is running , middleware are working properly

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+//test object
 const app = express();
 
-app.listen(8000,()=>{console.log(`server running on port ${8000}`)});
+//dot env configuration
+dotenv.config();
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{console.log(`server running on port ${8000}`.bgBlue)});
